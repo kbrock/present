7c1b4a657d8e018a229edfc7232288542112f606
Keenan Brock <keenan@thebrocks.net>
2013-04-13 09:14:38 -0400

    initial rails postgres mv README.rdoc README.md rm app/assets/images/rails.png

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Present::Application.config.secret_key_base = '10ec487ee66a69660f29ffa4136b0f4f50c59c9a761f29a3367608187b31ab44fe396465e83efd1a3e9bbcee2cf18c5d5ca68f86c3a55a12a3cb5e047bf29f3e'
